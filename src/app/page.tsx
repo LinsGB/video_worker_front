742a02f45cf9a34202f46223723e3c1a1641c871
Gabriel Lins <linsgb2@gmail.com>
2024-05-31 01:07:47 -0300

    chore - adding no check to go faster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+//@ts-nocheck
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useEffect, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
@@ -12,12 +13,14 @@ export default function Home() {
   }, [])
 
 
-  const setUrl = (event) => {
+  const setUrl = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    //@ts-ignore
     const formData = new FormData(event.target);
     const payload = Object.fromEntries(formData.entries());
     axios.post('http://localhost:8000/set_url',payload).then(() => {
       if(audioRef.current) {
+        //@ts-ignore
         audioRef.current.load()
       }
     })
@@ -29,8 +32,8 @@ export default function Home() {
         <textarea name="url" ></textarea>
         <button type="submit">Salver URL</button>
       </form>
-        <audio controls ref={audioRef}> <source src="http://localhost:8000/original_audio" type="audio/mpeg"/></audio>
-        <audio controls ref={audioRef}> <source src="http://localhost:8000/translated_audio" type="audio/mpeg"/></audio>
+        <audio controls ref={audioRef}> <source src={`${process.env.NEXT_PUBLIC_API_URL}original_audio`} type="audio/mpeg"/></audio>
+        <audio controls ref={audioRef}> <source src={`${process.env.NEXT_PUBLIC_API_URL}translated_audio`} type="audio/mpeg"/></audio>
     </div>
   );
 }
